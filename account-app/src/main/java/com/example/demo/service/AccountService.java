71e187a1a5e9f86ac44221b729226b1db5a29a15
Sourabh-Haldar <107604525+Sourabh-Haldar@users.noreply.github.com>
2024-01-25 15:59:40 +0530

    Add files via upload

---
+++ account-app/src/main/java/com/example/demo/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.exception.AccountNotFoundException;
+import com.example.demo.model.Account;
+
+public interface AccountService {
+	Account createAccount(Account account);
+
+	List<Account> getAllAccounts();
+	Account getAccountByaccountNumber(String accountNumber) throws AccountNotFoundException;
+	Account updateAccountByAccountNumber(String accountNumber,Account account)throws com.example.demo.exception.AccountNotFoundException;
+	public void deleteAccountByAccountNumber(String accountNumber)	throws	AccountNotFoundException;
+	Account	getAccountByEmail(String email)	throws	AccountNotFoundException;
+}
